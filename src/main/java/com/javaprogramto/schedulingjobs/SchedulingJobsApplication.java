8aec4d62ad139749540a5cc68db43cf61451a188
Venkatesh Nukala <49858284+java-w3schools@users.noreply.github.com>
2020-04-25 23:50:17 +0530

    initial base commit

---
+++ src/main/java/com/javaprogramto/schedulingjobs/SchedulingJobsApplication.java
@@ -0,0 +1,13 @@
+package com.javaprogramto.schedulingjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingJobsApplication.class, args);
+	}
+
+}
